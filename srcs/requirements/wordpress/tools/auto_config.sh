0e986d157f2aed3c679d69b6aca9d8bf92f86845
gotgotd <daigneau.gautier@gmail.com>
2024-12-09 15:11:21 +0100

    cleaned auto_config.sh

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -9,18 +9,7 @@ done
 # Navigate to the WordPress installation directory
 cd /var/www/html
 
-# OPTION 1 : Force the download of wordpress
-echo "Forcing WordPress core files download..."
-rm -rf /var/www/html/*
-./wp-cli.phar core download --allow-root
-
-# OPTION 2 : cleaning up DataBase before using
-if ./wp-cli.phar core is-installed --allow-root; then
-    echo "Cleaning up existing WordPress installation..."
-    ./wp-cli.phar db reset --yes --allow-root
-fi
-
-# Check if WP-CLI is already installed
+# Ensure WP-CLI is installed
 if [ ! -f wp-cli.phar ]; then
     echo "Downloading WP-CLI..."
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -29,20 +18,26 @@ else
     echo "WP-CLI is already installed."
 fi
 
-# Download WordPress core files if not already present
+# Ensure WordPress core files are downloaded
 if [ ! -f wp-includes/version.php ]; then
-    echo "Downloading WordPress core..."
+    echo "Downloading WordPress core files..."
     ./wp-cli.phar core download --allow-root
 else
     echo "WordPress core files are already present."
 fi
 
-# Create the wp-config.php file if it doesn’t exist
+# Ensure wp-config.php is created
 if [ ! -f wp-config.php ]; then
     echo "Creating wp-config.php..."
     ./wp-cli.phar config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb:3306 --allow-root
 else
     echo "wp-config.php already exists."
+fi
+
+# Reset the database if WordPress is already installed
+if ./wp-cli.phar core is-installed --allow-root; then
+    echo "Resetting existing WordPress installation..."
+    ./wp-cli.phar db reset --yes --allow-root
 fi
 
 # Install WordPress if not already installed
@@ -53,7 +48,7 @@ else
     echo "WordPress is already installed."
 fi
 
-# Add a new user only if it doesn't exist
+# Create a WordPress user if it doesn't already exist
 if ! ./wp-cli.phar user get ${USER_ID} --allow-root > /dev/null 2>&1; then
     echo "Creating a new WordPress user..."
     ./wp-cli.phar user create ${USER_ID} ${USER_EMAIL} --role=editor --user_pass=${USER_PASSWORD} --allow-root
@@ -61,7 +56,7 @@ else
     echo "WordPress user ${USER_ID} already exists."
 fi
 
-
 # Start PHP-FPM
 echo "Starting PHP-FPM..."
 php-fpm7.3 -F
+
